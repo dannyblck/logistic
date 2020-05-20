819aa99bb329782ec5670bbabd88604ffa3e38a7
dannyblck <Amirkhanov.Official@gmail.com>
2020-05-20 11:45:11 +0600

    carousel ready

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.reviews_carousel').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrowleft_reviews.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrowright_reviews.svg"></button>',
+    });
+});
